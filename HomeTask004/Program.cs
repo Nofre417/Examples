9f00d31e884607dff1b6604c8e164d1150ee6c52
David Movsesian <nofrevideo@gmail.com>
2023-07-05 14:26:01 -0600

    Задача 8. Перечислить четные числа от 0 до N

---
+++ HomeTask004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();   
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+
+while(count < number){
+    count++;
+    if (count % 2 == 0) Console.WriteLine(count  +" ");
+}
