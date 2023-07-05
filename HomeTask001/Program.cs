65d3818cf223e4b3e3242e0406f462da3e50dcc3
David Movsesian <nofrevideo@gmail.com>
2023-07-05 14:03:41 -0600

    Задача 2. Вывести большее и меньшее число

---
+++ HomeTask001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите вервое число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB) Console.WriteLine($"Число {numberA} больше числа {numberB}");
+else if (numberA < numberB) Console.WriteLine($"Число {numberB} больше числа {numberA}");
+else{
+    Console.WriteLine($"Число {numberA} равно {numberB}");
+}
