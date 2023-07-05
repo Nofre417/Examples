8364d96cbc0851a9b7fed12462551fc06e589a8c
David Movsesian <nofrevideo@gmail.com>
2023-07-05 14:25:24 -0600

    Задача 6. Проверка числа на четность 2

---
+++ HomeTask003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число для проверки: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)  Console.WriteLine("Число делиться на два без остатка");
+else Console.WriteLine("Число не делиться на два без остатка");
