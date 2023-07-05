75d19b5a9354ec97a37a6107d562a06239235d44
David Movsesian <nofrevideo@gmail.com>
2023-07-04 22:12:24 -0600

    Добавили функцию рандомного числа и добавлили более красивый вывод

--- Example003_IntegerNumber/Program.cs
+++ Example003_IntegerNumber/Program.cs
@@ -1,3 +1,6 @@
-﻿int namberA = 3;
-int namberB = 5;
-Console.WriteLine(namberA + namberB);
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine("Первое число: " + numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine("Второе число: " + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
