5ad57ae72ac5a7dd1d6cf0230605dc5bdd076b0d
David Movsesian <nofrevideo@gmail.com>
2023-07-05 13:44:24 -0600

    Четвертая задача

---
+++ Task004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = number  - (number*2) - 1;
+
+while(number != sum){
+    Console.Write(" " + number);
+    number--;
+}
