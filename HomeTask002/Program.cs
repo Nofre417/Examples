b76bb2935087cdf5a2cd2231544e8715d6f9e645
David Movsesian <nofrevideo@gmail.com>
2023-07-05 14:24:31 -0600

    Задача 4. Вывксти максимальное число из 3 вводных

---
+++ HomeTask002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите вервое число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC) {Console.WriteLine($"Число {numberA} максимальное из данных");}
+if (numberB > numberA && numberB > numberC) {Console.WriteLine($"Число {numberB} максимальное из данных");}
+if (numberC > numberA && numberC > numberB) {Console.WriteLine($"Число {numberC} максимальное из данных");}
