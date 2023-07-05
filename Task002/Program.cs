47036617e3663cc1481718f15c5b28f8aea30a59
David Movsesian <nofrevideo@gmail.com>
2023-07-05 13:43:55 -0600

    Вторая зачача

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int sqr = numberA / numberB;
+
+if (sqr == numberB)
+{
+    Console.WriteLine("Первое числа являеться квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не являеться квадратом второго числа");
+}
