8c168ceb392cbb1f5b5f4e417a23630cf8c48e3f
David Movsesian <nofrevideo@gmail.com>
2023-07-04 22:23:34 -0600

    Сделали приветсвие к определному пользователю

---
+++ Example005_HelloDavid/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+ 
+if (username.ToLower() == "david")
+{
+    Console.WriteLine("Салют, рад что ты вернулся " + username);
+}
+else
+{
+    Console.WriteLine("Добро пожаловать " + username);
+}
