46207c59d0ddacb366dcbe9109481bdc1fa8e9ae
Nofre417 <nofrevideo@gmail.com>
2023-07-28 05:00:44 +0200

    Задача 6

---
+++ Task006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int count = 0;
+
+while(count != 5)
+{
+int number = new Random().Next(10, 100);
+
+int a1 = number / 10;
+int a2 = number % 10;
+
+if (a1>a2)
+{
+    WriteLine("First way");
+    WriteLine($"{number}->{a1}");
+}
+else
+{
+    WriteLine("Second way");
+    WriteLine($"{number}->{a2}");
+}
+count++;
+}
