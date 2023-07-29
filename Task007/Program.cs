ba6e992edac03d14bc8931d9bcd33d1ffb298e1f
Nofre417 <nofrevideo@gmail.com>
2023-07-29 16:07:27 +0200

    Задача 3. Семинар

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,2 +1,16 @@
-﻿// Тест команда
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Clear();
+
+int[] numbers = new int [] {2, 5, 13, 7, 6, 4};
+
+int size = 6, sum = 0, avg = 0, index = 0;
+
+while(index < size)
+{
+    sum = sum + numbers[index];
+    index++;
+}
+avg = sum / size;
+
+Console.WriteLine($"Answer: {avg}");
